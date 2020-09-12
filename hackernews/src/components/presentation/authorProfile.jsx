bec95f9d410cf136b443a9b6991860ae6a33d4ab
Aaron <aarongillies88@gmail.com>
2020-09-12 12:50:23 -0400

    added skeleton component for authorProfile

---
+++ hackernews/src/components/presentation/authorProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function authorProfile() {
+  // This component will be strictly presentational, will recieve props from main.jsx
+  // and render the UI for the author clicked on in the stories/storyCard components.
+  return (
+    <div>
+      <h1>UI for author profile will live here</h1>
+    </div>
+  );
+}
+
+export default authorProfile;
