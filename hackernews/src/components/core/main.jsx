611bc305e223662c14fd60e569791e981f5d35a9
Aaron <aarongillies88@gmail.com>
2020-09-14 16:35:59 -0400

    making base api call in main, working on rendering top stories via id

--- hackernews/src/components/core/main.jsx
+++ hackernews/src/components/core/main.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// Component imports
+import Stories from "../presentational/Stories";
 
 function Main() {
-  // API call to HN probably happens here
+  // Component state
+  const [stories, setStories] = useState([]);
+  // API call on mount
+  useEffect(() => {
+    axios
+      .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      .then((res) => {
+        setStories(res.data);
+      });
+  }, []);
+
+  console.log(stories);
   // This component will render out cards for news articles (props will have to be passed down into the cards)
   return (
     <div>
       <h1>News articles and access to additional information will live here</h1>
+      <Stories data={stories} />
     </div>
   );
 }
